afe95c1b1dc62bd43b8140a2cb39095008c716b2
nalezyty <nalezyty@mit.edu>
2015-06-03 09:41:55 -0400

    add the deploy script

---
+++ deploy-ghpages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf out || exit 0;
+mkdir out;
+
+GH_REPO="@github.com:nalezyty/built-artifacts-oidc.git"
+
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+done
+
+git init
+git config user.name "nalezyty-travis"
+git config user.email "travis"
+
+git add .
+git commit -m "deployed to github pages"
+git push --force --quiet $FULL_REPO master:gh-pages
