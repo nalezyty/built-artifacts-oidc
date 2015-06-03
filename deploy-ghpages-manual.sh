5881b57d44af15468c8fb7080f032f765703dae4
nalezyty-travis <travis>
2015-06-03 11:32:09 -0400

    deployed to github pages

--- deploy-ghpages-manual.sh
+++ deploy-ghpages-manual.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-GH_REPO="@github.com:nalezyty/built-artifacts-oidc.git"
+GH_REPO="@github.com/nalezyty/built-artifacts-oidc.git"
 GH_TOKEN="c47e16dbfd0d3980a4d349ef92bebaa614933374"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
-git clone $FULL_REPO
+git init
 git config user.name "nalezyty-travis"
 git config user.email "travis"
 
