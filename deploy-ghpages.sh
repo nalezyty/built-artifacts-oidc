a5921a02cd5babcbd6823ddced6fd372c08d0f5b
nalezyty-travis <travis>
2015-06-03 13:42:50 -0400

    deploy only the war file to git

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -5,11 +5,12 @@ GH_REPO="@github.com/nalezyty/built-artifacts-oidc.git"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
+
 git checkout master
 git init
 git config user.name "nalezyty-travis"
 git config user.email "travis"
 
-git add .
-git commit -m "deployed to github pages"
+git add oidc-mitoverlay/target/my-openid-connect-server.war
+git commit -m "deployed war file to github pages"
 git push --force --quiet $FULL_REPO master
